04a7245fc8070f93a4a9ceca0aab8d29621de43b
Martin Vazquez Perez <martinvp2003@outlook.com.ar>
2023-11-16 21:03:37 -0300

    servidor funcionando

--- ProyectoFinal/AppCoder/views.py
+++ ProyectoFinal/AppCoder/views.py
@@ -1,10 +1,10 @@
+# Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
-from AppCoder.urls import view_cursos,view_inicio
-# Create your views here.
-
-def view_inicio(request):
-    return HttpResponse("bienvenidos!!!!!!!!!!!!!")
+from AppCoder.urls import view_cursos, view_inicio
 
 def view_cursos(request):
     return render(request,"templates/AppCoder/padre.html")
+
+def view_inicio(xx):
+    return HttpResponse("bienvenidos!!!!!!!!!!!!!")
