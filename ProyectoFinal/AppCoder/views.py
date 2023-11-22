9b7eb349f1df6301ba924eace5827482a79fe4c3
Martin Vazquez Perez <martinvp2003@outlook.com.ar>
2023-11-21 21:01:58 -0300

    servidor funcionando

--- ProyectoFinal/AppCoder/views.py
+++ ProyectoFinal/AppCoder/views.py
@@ -2,18 +2,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Template, Context
+from datetime import datetime
 
 def view_cursos(request):
     #return render(request,"templates/AppCoder/padre.html")
     nom = "martin"
     ap = "vazquez perez"
-    ruta = r"C:\Users\josem\OneDrive\Escritorio\proyecto final\ProyectoFinal\AppCoder\templates\AppCoder\padre.html"
-    diccionario = {"nombre": nom, "apellido": ap}
-    mi_archivo = open(ruta,"r",encoding="utf-8")
-    plantilla = Template(mi_archivo.read())
-    contexto = Context(diccionario)
-    documento = plantilla.render(contexto)
-    return HttpResponse(documento)
+    ahora = datetime.now()
+    diccionario = {"nombre": nom, "apellido": ap, "nacionalidad":"argentino","hora": ahora, "ciudades": ['lima', 'san pablo']}
+    return render(request,"AppCoder/padre.html",context=diccionario)
         
 
 def view_inicio(xx):
